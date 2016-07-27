cbc239cdd63fbbc3641eb0e9da2802ab17a0e353
abelmaniquis <abelmaniquis@gmail.com>
2016-07-27 21:13:55 +0000

    app.get uploads html files

---
+++ routes.js
@@ -0,0 +1,3 @@
+module.exports = function(app){
+  app.use(express.static('public'));
+}
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Set up
 var express = require("express");
 var app = express();
@@ -8,13 +7,30 @@ var passport = require("passport");
 var bodyParser = require("body-parser");
 var configDB = require('./config/database.js');
 var io = require('socket.io');
+var path =require('path');
 
 //configuration
 mongoose.connect(configDB.url);
 
-app.listen(port);
-console.log('Listening on port: ' + port);
 //Should load login page by default
 
 //Routing
-app.use(express.static('public'));
+//app.use(express.static('public'));
+//https://scotch.io/tutorials/use-expressjs-to-deliver-html-files
+app.get("/",function(req,res){
+  //load login page by default
+  res.sendFile(path.join(__dirname + '/public/login.html'));
+})
+
+app.get("/chat",function(req,res){
+  //load chatpage
+  //should only be accesible by password
+  res.sendFile(path.join(__dirname + '/public/chat.html'));
+});
+
+app.get("/signup",function(req,res){
+  res.sendFile(path.join(__dirname + '/public/signup.html'));
+});
+
+app.listen(port);
+console.log('Listening on port: ' + port);
