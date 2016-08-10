ee0dad0628a372995eaee6bf7eae3ea9a54e6819
abelmaniquis <abelmaniquis@gmail.com>
2016-08-10 20:25:12 +0000

    chat works on front end

--- client/views/scripts/chat.js
+++ client/views/scripts/chat.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
     //var socket = io();
     var input = $(".inputMessage");
     var chatPage = $('#chatPage');
-    var messages = $('messages');
+    var messages = $('.messages');
     var addMessage = function(message){
       messages.append('<ul>' + message + '</ul>');  
     };
     
     input.on('keydown',function(event){
-        addMessage;
-        console.log("I see this");
+        if (event.keyCode !=13){
+            return;
+        }
+        var message = input.val();
+        addMessage("USERNAME: " + message);
+        input.val('');
     })
     
 });
