3d0cd4304797adca803c7b394220d948e8f2dc42
abelmaniquis <abelmaniquis@gmail.com>
2017-01-09 21:52:28 +0000

    fixed some margins

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,12 @@
 module.exports = {
   db:{
-  url:process.env.MONGODB_URI
-  //url:'mongodb://localhost/groveUsers'
+  url:process.env.MOGODB_URI
   }
 };
 
 //'url':'mongodb://localhost/groveUsers'
+
+/*
+deployment:
+url:process.env.MONGODB_URI
+*/
